23a85b3ebfc9bf205aa0aae018a199000bfb5898
wanhenri <you@example.com>
2020-04-06 19:13:01 -0300

    [fix](frontend):acrescimo de figuras e textos

--- frontend/src/pages/About/index.js
+++ frontend/src/pages/About/index.js
@@ -1,66 +1,79 @@
 import React from 'react';
 import './styles.css';
 
+import Etec from "../../img/About/etec_santana_de_castro.gif"
+import Unitau from "../../img/About/unitau-logo-1.png"
+import { FaUserGraduate } from 'react-icons/fa'
+import { TiGroup } from 'react-icons/ti'
+import { DiTerminal } from 'react-icons/di'
+
 export default function About() {
     return (
         <div className="about-container">
         <div className="content">
-               <h1>Olá! Bem-vindo!</h1>
-               <br />
+            <section>
+                <h1>Resumo de qualificações e trajetória de carreira</h1>
                 <p>
-                    Agradeço por ter chego até ao meu pequeno e humilde espaço, onde compartilho um pouco
-                    da minha experiência, um pouco da minha vida e, claro, vendo um pouco dos produtos
-                    que desenvolvo.
-                </p>
-                <br />
-                <p>
-                    Sou Formado em Engenharia Mecânica e Técnico Mecânico, com Pós-Graduação em Engenharia 
-                    da Qualidade Lean Seis Sigma e Certificação Green Belt. Já trabalhei durante 6 meses como 
-                    Engenheiro Mecânico e Engenheiro de Qualidade Autônomo no desenvolvimento um Projeto de 
-                    instalação do ar condicionado, depois em projetos e em projetos de Melhoria Continua.
-                </p>
-                <br />
-                <p>
-                    Atualmente trabalho no Instituto Nacional de Pesquisas Espaciais (INPE), na Divisão de Modelagem 
-                    e Desenvolvimento, na avaliação e validação do modelo numérico de previsão.
-                    Principais atividades: dar suporte para os pesquisadores nas simulações de Tempo e Clima
-                    utilizando algumas resoluções do modelo global de previsão numérica, na manipulação e tratamento
-                    dos dados, na geração de campos espaciais e na realização de análises estatísticas. 
-                </p>
-                <p>
-                    Com essa minha entrada para o Instituto Nacional de Pesquisas Espaciais no final de 2015, pude ter mais contato com o mundo 
-                    da tecnologia e suas maravilhas. Porém, a minha área de atuação dentro do Instituto estava longe de ser no setor de Backend
-                    ou Front-end. Trabalhei na área de validação do modelo Global (BAM) e com isso pude desenvolver e participar de diversos projetos.
-                </p>
-                <br />
-                <p>
-                    [ CASES DE SUCESSO NA ÁREA DE METEOROLOGIA]
-                </p>
-                <br />
-                <p>
-                    Mas não parou por ai!
-                </p>
-                <br />
-                <p>
-                    No meados de 2018, tive os prazer de conhecer esse universo do Desenvolvimento Web e com ele suas magias. 
-                    Os meus primeiros passos foram adquirir alguns cursos online introdutório sobre o assunto; também acompanhei 
-                    muitos canais no youtube focado em tecnologia; diversos grupos no whatsapp e também no Telegram. Como
-                    pode ver, o conteúdo (gratuito ou não) é fasto para esse universo de tecnologia web.
-                </p>
-                <br />
-                <p>
-                    Ainda em 2018 foquei nos estudos em Python paralelamente aos estudos de Desenvolvimento Web. De fato, me dediquei
-                    mais em Python do que Dev Web, é a vida.
-                    No ano de 2019, surgiu a oportunidade de continuar os estudos de Desenvolvimento Web, porém muita duvida foram surgindo.
-                    Recorri a ‘ajudo dos universitários’. Fui atrás de aulas particulares para sanar todos esses pontos [colocar o linkedin do matheus] 
-                    e tudo começou a andar.
-                    Hoje, ano de 2020, graças a muita dedicação e muitas aulas, consigo apresentar alguns projetos-teste e projetos ‘da vida real’.
-                </p>
-                <br />
-                <p>
-                    [ CASES DE SUCESSO NA ÁREA DE DESENVOLVIMENTO]
-                </p>
-
+                    Formado em Engenharia Mecânica e Técnico Mecânico, com Pós-Graduação em Engenharia da Qualidade 
+                    Lean Seis Sigma e Certificação Green Belt. Já trabalhei durante 6 meses como Engenheiro Mecânico e 
+                    Engenheiro de Qualidade Autônomo no desenvolvimento um Projeto de instalação do ar condicionado, depois 
+                    em projetos e em projetos de Melhoria Continua. Trabalho há 4 anos no Instituto Nacional de Pesquisas Espaciais (INPE),
+                     na Divisão de Modelagem e Desenvolvimento, na avaliação e validação do modelo numérico de previsão. </p>
+                     <p>Principais atividades:</p> 
+                     <ul>
+                         <li>Dar suporte para os pesquisadores e tecnologistas</li>
+                         <li>Simulações de Tempo e Clima utilizando algumas resoluções do modelo global de previsão numérica;</li>
+                         <li>Resoluções:</li>
+                         <ul>
+                             <li>TQ299L64</li>
+                             <li>TQ666L64</li>
+                             <li>TQ0062L28</li>
+                         </ul>
+                         <li>Manipulação e tratamento dos dados</li>
+                         <li>Geração de campos espaciais</li>
+                         <li>Realização de análises estatísticas</li>
+                     </ul>                      
+            </section>
+            <section>
+                <h1>Formação</h1>
+                <img 
+                    src={Unitau} 
+                    alt="Logo unitau"                                     
+                    width="55" 
+                    height="55" 
+                />
+                <h2>Universidade de Taubate</h2>
+                                    
+                <FaUserGraduate size={18}/><span> Pós-Graduação em Engenharia de Qualidade Lean Seis Sigma Green Belt</span>
+                <p>alguma descrição do curso</p>
+                <FaUserGraduate size={18}/><span> Engenharia Mecânica</span>
+                <p>alguma descrição do curso</p>
+                
+                <img 
+                    src={Etec} 
+                    alt="Logo unitau"                                     
+                    width="205" 
+                    height="85" 
+                />
+                
+                <h2>Etec Prof. José Sant'Ana de Castro</h2>
+                <FaUserGraduate size={18}/><span> Técnico Mecânico</span>
+                <p>alguma descrição do curso</p>
+                
+            </section>
+            <section>
+                <h1>Qualificações</h1>
+                <DiTerminal size={38}/><span> Quatro anos de experiência com linguagens de programação</span><br />
+                <TiGroup size={38}/><span>  Facilidade de trabalhar em equipe</span>
+                <ul>
+                    <li>Um pouco mais que seis meses de experiência na área de Engenharia</li>
+                    <li>Quatro anos de experiência com linguagens de programação</li>
+                    <li>Conhecimentos em Front-end, Análise de dados, Ferramentas de gestão e softwares de modelagem mecânica</li>
+                    <li>Desenvolvimento de duas ferramentas de plotagem e analise estatística, SCANPLOT e PrecAnalise, para dados de Precipitação (chuva)</li>
+                    <li>Elevada capacidade de organização e pro atividade.</li>
+                    
+                </ul>                
+            </section>
         </div>
     </div>
     );
--- frontend/src/pages/Projects/index.js
+++ frontend/src/pages/Projects/index.js
@@ -1,13 +1,157 @@
 import React from 'react';
 import './styles.css';
 
+const HabilidadeLinguagens = [
+    {
+        Habilidade:[
+            "JavaScript", 
+            "Python",
+            "C++",
+            "VBA",
+            "Fortran" 
+        ]
+    },
+];
+
+const Frameworks = [
+    { 
+        Frameworks:
+            "react.js"            
+    },
+];
+
+const GerenciamentoProjeto = [
+    { 
+        GerenciamentoDeProjetos: [
+            "Readmine" ,
+            "Ms Project"
+        ]
+    },
+];
+
+const ControleVersao = [
+    { 
+        ControleDeVersao: [
+            "SVN",
+            "Git" 
+        ]
+    },
+];
+
+const DesenvolvimentoFluxograma = [
+    {
+        DesenvolvimentoFluxograma: [ 
+            "Draw.io",
+            "Bizagi"
+        ]  
+    },
+];
+
+const Prototipagem = [
+    {
+        Prototipagem: [ 
+            "Figma"
+        ]  
+    },
+];
+
+const AnaliseProcesso = [
+    { 
+        AnaliseProcess: [
+            "PCDA",
+            "Seis Sigma", 
+            "Fluxograma de Processa", 
+            "Histograma",
+            "Diagrama de Pareto",
+            "Diagrama de Ishikawa",
+            "Cartas de Controle",
+            "Folha de Verificação",
+            "Grafico de dispersão",
+            "Controle Estatistico de Processos",
+            "5W2H",
+            "PDSA",
+            "5S",
+            "Seis Sigma"
+        ]
+    },
+];
+
+const SeisSigma = [
+    {
+        SigmaDetalhe:[
+            "Preciso Detalhar"
+        ]
+    },
+];
+
+const FerramentaEstatistica = [
+    {
+        Estatistica: [ 
+            "Minitab"
+        ]  
+    },
+];
+
+const SoftwareModelagem = [
+    {
+        Software: [ 
+            "AutoCAD 2D",
+            "AutoCAD 3D",
+            "AutoDesk Inventor"
+        ]  
+    },
+];
+
+
 export default function Projects() {
     return (
         <div className="projects-container">
-        <div className="content">
-               <h1>Projects</h1>
-                <p>Nessa região acrescenta o mapa onde que apresenta a região do escritório</p>
+            <div className="content">
+                <table>
+                    <caption>Habilidades</caption>
+                    <thead>
+                        <tr>
+                            <th>
+                                <button type="button">Habilidades/Linguagens</button>
+                            </th>
+                            <th>
+                                <button type="button">Frameworks</button>
+                            </th>
+                            <th>
+                                <button type="button">Gerenciamento de Projetos</button>
+                            </th>
+                            <th>
+                                <button type="button">Controle de Versão</button>
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+                <table>
+                    <caption>Habilidades</caption>
+                    <thead>
+                        <tr>
+                            <th>
+                                <button type="button">Desenvolvimento de Fluxograma</button>
+                            </th>
+                            <th>
+                                <button type="button">Ferramentas da Qualidade</button>
+                            </th>                 
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
-    </div>
     );
 }
+
+//export default function Projects() {
+//    return (
+//        <>
+//            <ProductTable
+//                products={[
+//                    {id: 1, habilidade: 'JavaScript', Frameworks: 'react.js', GerenciamentoDeProjetos: 'Readmine', ControleDeVersao: 'SVN' }
+//                ]}
+//            />
+//        </>
+//    );
+//}
